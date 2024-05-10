0d428901923b55f80ebb47caa9ddd163e3acadc2
Trabalho em casa <166721572+RPAVelasco@users.noreply.github.com>
2024-05-10 09:48:24 -0300

    LoginActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/LoginActivity.kt
@@ -7,28 +7,35 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.restaurante.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-//Criação da variável binding que servirá para manipular a tela.
-    private lateinit var binding:ActivityLoginBinding
+    // Criação da variável binding que servirá para manipular a tela.
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Infla o layout da atividade de login usando o binding gerado pelo Data Binding.
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configura o listener de clique para o botão de login.
         binding.buttonEntrar.setOnClickListener {
+            // Obtém o nome de usuário e senha digitados e remove espaços em branco.
             val username = binding.editUsername.text.toString().trim()
             val password = binding.editPassword.text.toString().trim()
 
-            if (username.equals("abc") && password.equals("123")) {
+            // Verifica se o nome de usuário e a senha correspondem aos valores esperados.
+            if (username == "abc" && password == "123") {
+                // Se as credenciais estiverem corretas, cria uma Intent para iniciar a MainActivity.
                 val i = Intent(this, MainActivity::class.java)
+                // Passa o nome de usuário como um extra para a próxima atividade.
                 i.putExtra("username", username)
                 startActivity(i)
+                // Finaliza a atividade de login para que o usuário não possa retornar pressionando o botão "Voltar".
                 finish()
             } else {
-                Toast.makeText(applicationContext, "Errou", Toast.LENGTH_LONG).show()
+                // Se as credenciais estiverem incorretas, exibe uma mensagem de erro.
+                Toast.makeText(applicationContext, "Erro", Toast.LENGTH_LONG).show()
             }
         }
-
     }
 }
